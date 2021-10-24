be8bbbdbb7edd39e700e1c24351978de43dec80f
Tcumulus <maarten.laureyssen@gmail.com>
2021-10-24 20:39:53 +0200

    Output

---
+++ precip_map_displayer.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+'''
+import cartopy.crs as crs
+import cartopy.feature as cfeature
+
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, projection=crs.PlateCarree())
+
+ax.add_feature(cfeature.BORDERS)
+gl = ax.gridlines(draw_labels=True)
+gl.xlabels_top = False
+gl.ylabels_right = False
+
+extend = [4.5, 6.5, 49.25, 51.25]
+ax.set_extent([extend], crs=crs.PlateCarree())
+
+plt.show()
+'''
